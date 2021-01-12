1cc467386688d0448a26eb9cfe46481236877bb5
DickersonAJ <>
2021-01-12 15:50:02 -0500

    added merge and fixed an error I noticed from MyLinkedList

--- Radix.java
+++ Radix.java
@@ -1,6 +1,7 @@
 public class Radix{
 
   public static int nth(int n, int col){
+    n = Math.abs(n);
     for (int i = 0; i < col; i++) {
       n = n/10;
     }
@@ -9,8 +10,15 @@ public class Radix{
   }
 
   public static int length(int n){
+    n = Math.abs(n);
     String str = Integer.toString(n);
     return str.length();
+  }
+
+  public static void merge(MyLinkedList original, MyLinkedList[]buckets){
+    for (int i = 0; i < buckets.length; i++) {
+      original.extend(buckets[i]);
+    }
   }
 
 }
--- RadixTester.java
+++ RadixTester.java
@@ -5,7 +5,24 @@ public class RadixTester{
     System.out.println(Radix.nth(273,1));
     System.out.println(Radix.nth(273,2));
     System.out.println(Radix.length(123456789));
+    System.out.println(Radix.nth(-273,2));
+    System.out.println(Radix.length(-123456789));
 
+    MyLinkedList og = new MyLinkedList();
+    og.add("hello");
+    MyLinkedList chain = new MyLinkedList();
+    chain.add("one");
+    chain.add("two");
+    chain.add("three");
+    MyLinkedList[] chains = new MyLinkedList[10];
+    for (int i = 0; i < 10; i++){
+      chains[i] = new MyLinkedList();
+      chains[i].add(i+"");
+      chains[i].add("beep");
+      chains[i].add("bop");
+    }
+    Radix.merge(og, chains);
+    System.out.println(og.toString());
   }
 
 }
