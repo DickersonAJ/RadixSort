aaf228e7fae6d846133e04369de656077cfcbbc7
DickersonAJ <>
2021-01-13 16:16:12 -0500

    added merge for SortableLinkedList

--- Radix.java
+++ Radix.java
@@ -21,4 +21,10 @@ public class Radix{
     }
   }
 
+  public static void merge(SortableLinkedList original, SortableLinkedList[]buckets){
+    for (int i = 0; i < buckets.length; i++) {
+      original.extend(buckets[i]);
+    }
+  }
+
 }
--- RadixTester.java
+++ RadixTester.java
@@ -1,7 +1,7 @@
 public class RadixTester{
 
   public static void main(String[]args){
-    System.out.println(Radix.nth(273,0));
+    /*System.out.println(Radix.nth(273,0));
     System.out.println(Radix.nth(273,1));
     System.out.println(Radix.nth(273,2));
     System.out.println(Radix.length(123456789));
@@ -22,7 +22,24 @@ public class RadixTester{
       chains[i].add("bop");
     }
     Radix.merge(og, chains);
-    System.out.println(og.toString());
+    System.out.println(og.toString());*/
+
+  SortableLinkedList og = new SortableLinkedList();
+  og.add(100);
+  SortableLinkedList chain = new SortableLinkedList();
+  chain.add(1);
+  chain.add(2);
+  chain.add(3);
+  SortableLinkedList[] chains = new SortableLinkedList[10];
+  for (int i = 0; i < 10; i++){
+    chains[i] = new SortableLinkedList();
+    chains[i].add(i);
+    chains[i].add(3);
+    chains[i].add(0);
   }
+  Radix.merge(og, chains);
+  System.out.println(og.toString());
+
+}
 
 }
